581d5e4f04be0b39fb5aa785af5113edd87210ed
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-23 01:07:15 +0300

    state and styling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,9 @@
 import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <DataContext.Provider value={userData}>
-    < ProfilePage />
-    </DataContext.Provider>
+    <UserContext />
   )
 }
-
 export default App;
--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,3 +1,13 @@
 import React , {createContext, useContext } from 'react'
 
 const DataContext = React.createContext();
+
+function UserContext() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  return (
+    <DataContext.Provider value={userData}>
+      <ProfilePage />
+    </DataContext.Provider>
+  );
+}
+export default UserContext;
