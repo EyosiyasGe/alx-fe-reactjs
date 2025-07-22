7451f0447341ca2b1244760146d1dcd3b627d948
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-23 01:13:18 +0300

    state and styling

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,3 +1,5 @@
-import  {createContext} from 'react'
-const DataContext = React.createContext();
+import React from "react";
+import { createContext } from 'react'
+
+const DataContext = createContext();
 export default UserContext;
