4864b2eb955d2ef1f547ee4db076153c6800e66a
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-23 01:08:19 +0300

    state and styling

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -2,12 +2,5 @@ import React , {createContext, useContext } from 'react'
 
 const DataContext = React.createContext();
 
-function UserContext() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-  return (
-    <DataContext.Provider value={userData}>
-      <ProfilePage />
-    </DataContext.Provider>
-  );
-}
+
 export default UserContext;
