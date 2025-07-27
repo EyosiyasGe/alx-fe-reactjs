d56c13432110e877c6bdbacae28f18cd3fd597e7
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-27 14:14:37 +0300

    state and styling

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import AddRecipeForm from './Components/AddRecipeForm'
+import RecipeList from './Components/RecipeList'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <AddRecipeForm />
+       <RecipeList />
+      </div>
+    </>
+  )
+}
+
+export default App
