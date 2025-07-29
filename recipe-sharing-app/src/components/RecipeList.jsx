1b1648f7ebe1215d45269d4435fbd263275c0136
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-29 20:14:24 +0300

    react

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore';
 
-export default function RecipeList() {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
 
-  if (!filteredRecipes.length) {
-    return <p>No recipes found.</p>;
-  }
-
-  return (
-    <div>
-      {filteredRecipes.map((recipe) => (
-        <div key={recipe.id} style={{ marginBottom: '1rem' }}>
-          <h3>
-            <Link to={`/recipe/${recipe.id}`} style={{ textDecoration: 'none', color: 'blue' }}>
-              {recipe.title}
-            </Link>
-          </h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
