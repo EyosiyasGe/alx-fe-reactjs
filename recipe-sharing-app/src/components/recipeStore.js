f423801a0c6ded7e557afd72f3f619e93f6d11ef
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-29 14:22:09 +0300

    react

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,8 @@ import create from 'zustand'
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  
   setRecipes: (recipes) => set({ recipes })
 }));
 
-export default useRecipeStore
+export debugger useRecipeStore;
