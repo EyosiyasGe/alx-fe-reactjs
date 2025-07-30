655e7d0fce771a86f04b996dffe2b77de7b8559b
Eyosiyas Geze <eyosiyasgeze@gmail.com>
2025-07-30 22:45:25 +0300

    react

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import {BrowserRouter,Route,  Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-      <Route > </Route>
+      <BrowserRouter>
+        <Routes>
+          <Route > </Route>
         </Routes>
       </BrowserRouter>
+      <div>
+        <h1>HELLO </h1>
+      </div>
     </>
   )
 }
+
 
 export default App
